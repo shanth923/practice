959ca32f451d3482cde7628146e08d8ff30c043e
R Shantha Kumar <shanth923@achiever-117.local>
2019-12-03 09:20:30 +0530

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by R Shantha Kumar on 12/3/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
